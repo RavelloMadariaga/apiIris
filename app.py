d9861aef99c04e4c822ffb67764d848641628f3b
RavelloMadariaga <mi.ravello@duocuc.cl>
2023-06-27 12:35:02 -0600

    mensajes profe

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def predecir():
     Medidas= json['Medidas']
     clf= joblib.load('modelo_entrenado.pkl')
     prediction=clf.predict(Medidas)
-    return "con esos datos tienes una probabilidad de salir en el lugar: {0}: ".format(prediction)
+    return "Profesor Italo con esos datos tienes una probabilidad de salir en el lugar:    . {0} ".format(prediction)
 
 if __name__ == '__main__':
     app.run()
